5aaea858e05f4df4148c4b454b0735c0214a7175
mukade-daiou <honeyworks.hatsune39@gmail.com>
2019-10-27 19:29:02 +0900

    git rm

--- settings/setup.sql
+++
@@ -1,7 +0,0 @@
-create database pcp;
-use pcp;
-create table users(id int,pass varchar(20) default '',username varchar(20),grade int);
-insert into users(id,username,grade) VALUES(1,"user1",124);
-insert into users(id,username,grade) VALUES(1,"user2",125);
-insert into users(id,username,grade) VALUES(1,"mukade",123);
-insert into users(id,username,grade) VALUES(1,"OB",120);
